f6cd9cb1098eef593fa252d676cae12a1cd371b5
Brandon Valley <bavvh8@mst.edu>
2019-11-25 17:17:08 -0600

    get_first_speech_time() working

--- audio_edit_utils.py
+++ audio_edit_utils.py
@@ -1,17 +1,39 @@
 import subprocess
+import speech_recognition as sr
+from os import path
+from pydub import AudioSegment
 
 
-
-'''returns .wav file, no spaces in path'''
+'''returns .wav file, no spaces in path, will overwrite'''
 def get_audio_from_video(in_vid_path, out_audio_path):
-    cmd = 'ffmpeg -i ' + in_vid_path + ' -acodec pcm_s16le -ac 2 ' + out_audio_path
-    subprocess.call(cmd, shell = True)
+    cmd = 'ffmpeg -i ' + in_vid_path + ' -acodec pcm_s16le -ac 2 ' + out_audio_path + ' -y'
+    subprocess.call(cmd, shell = False)
+    
     
 '''takes .wav file, no spaces in path'''    
 def get_audio_duration(in_audio_path):
     cmd = 'ffprobe -i ' + in_audio_path + ' -show_entries format=duration -v quiet -of csv="p=0"'
-    d = subprocess.check_output(cmd, shell = True)
+    d = subprocess.check_output(cmd, shell = False)
     return float(d)
+
+
+'''takes .wav file, no spaces in path, will overwrite'''
+def clip_audio(in_audio_path, cliped_audio_path, start_time, end_time):
+    cmd = 'ffmpeg -i ' + in_audio_path + ' -ss ' + str(start_time) + ' -to ' + str(end_time) + ' -c copy ' + cliped_audio_path + ' -y'
+    subprocess.call(cmd, shell = False)
+
+
+# returns False if there is no speach in audio
+def transcribe_audio(in_audio_path):
+    # use the audio file as the audio source                                        
+    r = sr.Recognizer()
+    with sr.AudioFile(in_audio_path) as source:
+            audio = r.record(source)  # read the entire audio file                  
+    try:
+        return r.recognize_google(audio)
+    except sr.UnknownValueError:
+        return False
+
 
 
 
@@ -20,4 +42,5 @@ def get_audio_duration(in_audio_path):
 
 
 if __name__ == '__main__':
-    print(get_audio_duration("C:\\Users\\Brandon\\Documents\\Personal_Projects\\my_movie_tools_big_data\\Screen_trimmed.wav"))
+#     print(get_audio_duration("C:\\Users\\Brandon\\Documents\\Personal_Projects\\my_movie_tools_big_data\\Screen_trimmed.wav"))
+    print(transcribe_audio("C:\\Users\\Brandon\\Documents\\Personal_Projects\\my_movie_tools\\clipped_audio.wav"))
--- find_first_speach.py
+++ find_first_speach.py
@@ -1,38 +1,117 @@
-import speech_recognition as sr
-from os import path
-from pydub import AudioSegment
+import audio_edit_utils
+
+
+
+# import speech_recognition as sr
+# from os import path
+# from pydub import AudioSegment
+
+
+#     if not isinstance(actual_result, dict) or len(actual_result.get("alternative", [])) == 0: raise UnknownValueError()
+# speech_recognition.UnknownValueError
+
 
 import time
-# # convert mp3 file to wav                                                       
-# sound = AudioSegment.from_mp3("transcript.mp3")
+
+
+
+
+
+
+VID_FILE_PATH = "C:\\Users\\Brandon\\Documents\\Personal_Projects\\my_movie_tools_big_data\\dolbycanyon_outside.mkv"
+# VID_FILE_PATH = "C:\\Users\\Brandon\\Documents\\Personal_Projects\\my_movie_tools_big_data\\Screen.mkv"
+
+FULL_AUDIO_PATH = 'full_audio.wav'
+CLIPED_AUDIO_PATH = 'clipped_audio.wav'
+AUDIO_CLIP_TIME = 10 # number of seconds to divide audio into to check for speech
+
+
+
+
+
+# returns # of seconds after start_time before the first time someone speaks in the audio (.wav file)
+# returns False if there is no speech in audio clip
+def get_first_speech_time(full_audio_path, start_time):
+    audio_duration = audio_edit_utils.get_audio_duration(full_audio_path) # use !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    cur_time = start_time
+    keep_looping = True
+    
+    while(keep_looping):
+        # get end time for clipping audio
+        end_time = cur_time + AUDIO_CLIP_TIME
+        if end_time > audio_duration:
+            end_time = audio_duration
+            keep_looping = False
+            
+        # clip audio
+        audio_edit_utils.clip_audio(full_audio_path, CLIPED_AUDIO_PATH, cur_time, end_time)
+        
+        # transcribe audio
+        transcription = audio_edit_utils.transcribe_audio(CLIPED_AUDIO_PATH)
+        print(transcription) #`````````````````````````````````````````````````````````````````````````````````````````
+
+        # return time if speech was found in audio clip
+        if transcription != False:
+            return cur_time
+    
+        # set up for next loop if no speech was found in audio clip
+        cur_time = end_time
+    return False
+
+
+
+
+
+
+
+
+
+
+
+# extract audio from vid
+audio_edit_utils.get_audio_from_video(VID_FILE_PATH, FULL_AUDIO_PATH)
+
+first_speech_time = get_first_speech_time(FULL_AUDIO_PATH, 0)
+
+print(first_speech_time)
+
+
+
+
+# 
+# 
+# 
+# # # convert mp3 file to wav                                                       
+# sound = AudioSegment.from_mp3("C:\\Users\\Brandon\\Documents\\Personal_Projects\\my_movie_tools_big_data\\ste-021-dilan-herkunft.mp3")
 # sound.export("transcript.wav", format="wav")
-
-
-# transcribe audio file                                                         
-AUDIO_FILE = "C:\\Users\\Brandon\\Documents\\Personal_Projects\\my_movie_tools_big_data\\Screen_audio.wav"
-# AUDIO_FILE = "C:\\Users\\Brandon\\Documents\\Personal_Projects\\my_movie_tools_big_data\\youre-so-funny-1.wav"
-# AUDIO_FILE = "C:\\Users\\Brandon\\Documents\\Personal_Projects\\my_movie_tools_big_data\\file_example_WAV_1MG.wav"
-
-s = time.time()
-
-
-# use the audio file as the audio source                                        
-r = sr.Recognizer()
-with sr.AudioFile(AUDIO_FILE) as source:
-        audio = r.record(source)  # read the entire audio file                  
-
-        print("Transcription: " + r.recognize_google(audio))
-
-print(time.time() - s)
-        
-ffmpeg -i "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen_audio.wav" -ss 10 -to 15 -c copy "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen_audio_10-15.wav"
-        
-        
-        
-ffmpeg -i "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen_trimmed.mp4" -acodec pcm_s16le -ac 2 "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen_trimmed.wav"
-# ffmpeg -i "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen.mkv" -acodec pcm_s16le -ac 2 "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen_audio.wav"
-ffmpeg -i "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen_trimmed.wav" 2>&1 | grep Duration | sed 's/Duration: \(.*\), start/\1/g'
-ffprobe -i "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen_trimmed.wav" -show_entries format=duration -v quiet -of csv="p=0"
+# 
+# 
+# # transcribe audio file                                                         
+# AUDIO_FILE = "transcript.wav"
+# # AUDIO_FILE = "C:\\Users\\Brandon\\Documents\\Personal_Projects\\my_movie_tools_big_data\\Screen_audio.wav"
+# # AUDIO_FILE = "C:\\Users\\Brandon\\Documents\\Personal_Projects\\my_movie_tools_big_data\\youre-so-funny-1.wav"
+# # AUDIO_FILE = "C:\\Users\\Brandon\\Documents\\Personal_Projects\\my_movie_tools_big_data\\file_example_WAV_1MG.wav"
+# 
+# s = time.time()
+# 
+# 
+# # use the audio file as the audio source                                        
+# r = sr.Recognizer()
+# with sr.AudioFile(AUDIO_FILE) as source:
+#         audio = r.record(source)  # read the entire audio file                  
+# 
+#         print("Transcription: " + r.recognize_google(audio))
+# 
+# print(time.time() - s)
+#         
+# # ffmpeg -i "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen_audio.wav" -ss 10 -to 15 -c copy "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen_audio_10-15.wav"
+# #         
+# #         
+# #         
+# # ffmpeg -i "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen_trimmed.mp4" -acodec pcm_s16le -ac 2 "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen_trimmed.wav"
+# # # ffmpeg -i "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen.mkv" -acodec pcm_s16le -ac 2 "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen_audio.wav"
+# # ffmpeg -i "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen_trimmed.wav" 2>&1 | grep Duration | sed 's/Duration: \(.*\), start/\1/g'
+# # ffprobe -i "C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools_big_data\Screen_trimmed.wav" -show_entries format=duration -v quiet -of csv="p=0"
 
 
 
