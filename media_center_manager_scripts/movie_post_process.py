21f08e1a0a101b4dff183b20c482e35fc13e94cb
Brandon Valley <bavvh8@mst.edu>
2020-08-30 21:09:03 -0500

    added sm

---
+++ media_center_manager_scripts/movie_post_process.py
@@ -0,0 +1,9 @@
+
+
+from sms.file_system_utils import file_system_utils as fsu
+# import test
+
+movie_pwd = 'D:\\test\\Die Hard (1988) [720p]'
+
+
+print('here')
---
+++ media_center_manager_scripts/remove_metadata_from_all_mp4_files_below_this_dir.cmd
@@ -0,0 +1,1 @@
+echo %CD% > C:\Users\Brandon\Documents\Personal_Projects\my_movie_tools\media_center_manager_scripts\out.txt
