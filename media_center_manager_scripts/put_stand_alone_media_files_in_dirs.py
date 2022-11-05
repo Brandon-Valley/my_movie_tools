b7bd1acaccbab2cb52b181a1bd5c12a665d88710
Brandon Valley <bavvh8@mst.edu>
2022-11-05 08:37:59 -0500

    put_stand_alone_media_files_in_dirs() init

--- media_center_manager_scripts/put_stand_alone_media_files_in_dirs.py
+++ media_center_manager_scripts/put_stand_alone_media_files_in_dirs.py
@@ -1,11 +1,22 @@
-def get_non_nested_media_file_path_l(in_dir_path): 
-    ''' Returns list of abs path strings to every .mp4, .mkv, and .avi file under in_dir_path recursively'''
-    pattern_l = ('*.mp4', '*.mkv', "*.avi") # the tuple of file types
-    
-    media_file_path_l = []
-        
-    for pattern in pattern_l:
-        pattern_media_file_path_str_l = list(path_obj.__str__() for path_obj in Path(in_dir_path).rglob(pattern))
-        media_file_path_l.extend(pattern_media_file_path_str_l)
+import media_fsu
 
-    return(media_file_path_l) 
+def put_stand_alone_media_files_in_dirs(in_dir_path):
+    stand_alone_media_file_l = media_fsu.get_media_file_path_l(in_dir_path, recursive = False)
+    print(stand_alone_media_file_l)
+    
+    
+    
+    
+    
+    
+    
+    
+
+if __name__ == "__main__":
+    print('in main')
+
+    in_dir_path = "C:\\Users\\Brandon\\Documents\\Other\\temp\\test"
+    put_stand_alone_media_files_in_dirs(in_dir_path)
+    
+    print('end of main')
+    
