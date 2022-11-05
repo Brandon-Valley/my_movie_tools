098da446a7d158712137f460249ba4c7825b1af2
Brandon Valley <bavvh8@mst.edu>
2022-11-05 08:58:55 -0500

    put_stand_alone_media_files_in_dirs() works

--- media_center_manager_scripts/put_stand_alone_media_files_in_dirs.py
+++ media_center_manager_scripts/put_stand_alone_media_files_in_dirs.py
@@ -1,14 +1,24 @@
+from pathlib import Path
+import os
+
 import media_fsu
 
+from sms.file_system_utils import file_system_utils as fsu
+
 def put_stand_alone_media_files_in_dirs(in_dir_path):
-    stand_alone_media_file_l = media_fsu.get_media_file_path_l(in_dir_path, recursive = False)
-    print(stand_alone_media_file_l)
+    stand_alone_media_file_path_l = media_fsu.get_media_file_path_l(in_dir_path, recursive = False)
     
-    
-    
-    
-    
-    
+    for stand_alone_media_file_path in stand_alone_media_file_path_l:
+        new_dir_path = os.path.splitext(stand_alone_media_file_path)[0]
+        
+        fsu.make_dir_if_not_exist(new_dir_path)
+        
+        og_file_name_with_ext = Path(stand_alone_media_file_path).name
+        new_media_file_path = os.path.join(new_dir_path, og_file_name_with_ext)
+        
+        print("Copying {} to {}...".format(stand_alone_media_file_path, new_media_file_path))
+        fsu.copy_object_to_path(stand_alone_media_file_path, new_media_file_path)
+        
     
     
 
